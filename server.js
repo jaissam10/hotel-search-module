97b1bf31020fb36392f88a33f2f81ead024fcf0b
Sameer Jaiswal <sameer10101994@gmail.com>
2020-05-20 17:53:50 +0530

    server.js changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 let port = process.env.PORT || 8000;
 
